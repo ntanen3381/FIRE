290f5abeb9c2b7af549e59c24f0bbd0f4ebc332d
Noah <noahtanenholtz@gmail.com>
2017-11-30 20:13:47 +0000

    connecting to server

--- scanGimbal.js
+++ scanGimbal.js
@@ -1,10 +1,10 @@
 var noble = require('noble');
-var addressToTrack = '1fd5cbe68f13';
-var socket = require('socket.io-client')('http://django-env.mfuhrkczbq.us-east-1.elasticbeanstalk.com', {reconnect: true});
+var addressToTrack = 'acbc32977a7c';
+var url = 'http://django-env.nfjak4vxpm.us-west-2.elasticbeanstalk.com/indoor_mapping/loction_form/beacon1/5/6/';
 
-socket.on('connect', function () {
-       console.log('Connection established');
-});
+//socket.on('connect', function () {
+  //     console.log('Connection established');
+//});
 
 noble.state = "poweredOn";
 noble.on('stateChange', function(state){
@@ -14,14 +14,14 @@ noble.on('stateChange', function(state){
 noble.startScanning([], true);
  
 noble.on('discover', function(peripheral){
-    // if(peripheral.uuid == addressToTrack){
+  // if(peripheral.uuid == addressToTrack){
          var macAddress = peripheral.uuid;
   	 var rssi = peripheral.rssi;
  	 var localName = peripheral.advertisement.manufacturerData 
 	 var distance = calculateDistance(rssi);
 	 console.log('Device: ', macAddress, ' Distance: ', distance, ' ', localName, ' ', rssi);
 	 sendToServer(distance, macAddress);
-    // }
+  // }
 });
 
 function calculateDistance(rssi) {
@@ -44,7 +44,17 @@ function calculateDistance(rssi) {
 
 
 function sendToServer(distance, macAddress) {
-   socket.emit('deviceData', {pi: 11245, GimbalId: macAddress, Distance: distance});
+//   socket.emit('deviceData', {pi: 11245, GimbalId: macAddress, Distance: distance});
+   url += Math.round(distance) + '/beacon2/5/4/' + Math.round(distance) + '/beacon3/4/5/' + Math.round(distance) + '/location_form_no_interface';
+   var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  //Return the url part of the request object:
+  res.write(url);
+  res.end();
+}).listen(8080);
+
    var sleep = require('sleep');
    sleep.sleep(10);
 }
