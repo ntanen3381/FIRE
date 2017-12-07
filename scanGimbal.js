c232bf65e5707ea6621399a918bfccadde183c51
Noah <noahtanenholtz@gmail.com>
2017-12-07 18:50:51 +0000

    fixed average

--- scanGimbal.js
+++ scanGimbal.js
@@ -1,9 +1,10 @@
 var noble = require('noble');
 var uuid = 'ab6baf91449e489d9200f2e67e3d2df0';
-var url = 'http://django-env.nfjak4vxpm.us-west-2.elasticbeanstalk.com/indoor_mapping/location_form/pi1/1.00/1.00/';
+var url = 'http://django-env.nfjak4vxpm.us-west-2.elasticbeanstalk.com/indoor_mapping/location_form/pi3/5.00/4.00/';
 var exec = require('child_process').exec;
 var http = require('http');
 var sleep = require('sleep');
+var distances = [];
 
 noble.state = "poweredOn";
 noble.on('stateChange', function(state){
@@ -14,21 +15,32 @@ noble.on('stateChange', function(state){
 noble.startScanning([], true);
 
 noble.on('discover', function(peripheral){
-   	exec('date "+%S"', function(error, stdout) {
 		var dataPacket = "" + peripheral.advertisement.manufacturerData;
 		if (dataPacket != "undefined")
 		{
 			var packet = JSON.stringify(peripheral.advertisement.manufacturerData.toString('hex')).substr(9, 32);
-      			if(uuid == packet &&  stdout % 10 == 0) 
+      			if(uuid == packet) 
       			{
          			var macAddress = peripheral.uuid;
   	 			var rssi = peripheral.rssi;
-	 			var distance = calculateDistance(rssi);
+				var distance = calculateDistance(rssi);
+	 			distances.push(distance);
 	 			console.log('Device: ', macAddress, ' Distance: ', distance, ' ', uuid, ' ', rssi);
-	 			sendToServer(distance, macAddress);
+				exec('date "+%S"', function(error, stdout) {
+					if (stdout % 10 == 0)
+					{
+						var average = 0.0;
+						for (var i = 0; i < distances.length; i++)
+						{
+							average = average + distances[i];
+						}
+						average = average / distances.length;
+						average = average * 3.28; 
+	 					sendToServer(average, macAddress);
+					}
+				});
       			}
 		}
-   	});
 });
 
 function calculateDistance(rssi) {
@@ -45,8 +57,7 @@ var exec = require('child_process').exec;
     return Math.pow(ratio,10);
   }
   else {
-    var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
-	distance *= 3.28;    
+    var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;    
     return distance;
   }
 } 
@@ -55,6 +66,7 @@ function sendToServer(distance, macAddress) {
    url += distance.toFixed(2) + '/location_form_single/';
    exec('x-www-browser ' + url, function(err, stdout) {
    });
+   distances = [];
    sleep.sleep(5);
-   url = 'http://django-env.nfjak4vxpm.us-west-2.elasticbeanstalk.com/indoor_mapping/location_form/pi1/1.00/1.00/';
+   url = 'http://django-env.nfjak4vxpm.us-west-2.elasticbeanstalk.com/indoor_mapping/location_form/pi3/5.00/4.00/';
 }
